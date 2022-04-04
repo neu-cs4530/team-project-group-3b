be8590fb4ab5ee1ace081f7770102b7d1cbbcfe6
KennethCostich <57121111+KennethCostich@users.noreply.github.com>
2022-04-04 15:07:23 -0400

    Spotify Button Test with mocks.  Need to update for new button.

--- frontend/src/components/Spotify/SpotifyButton.test.tsx
+++ frontend/src/components/Spotify/SpotifyButton.test.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react';
 import '@testing-library/jest-dom'
+import  userEvent from "@testing-library/user-event";
 import { render, RenderResult } from '@testing-library/react';
 import SpotifyButton from './SpotifyButton';
 import TownsServiceClient from '../../classes/TownsServiceClient';
@@ -31,7 +32,11 @@ describe("SpotifyButton", () => {
     });
 
     it("makes a call to apiClient.requestSpotifyAuthorizationFlow when clicked", () => {
-        
+        expect(mockRequestSpotifyAuthorizationFlow).not.toBeCalled();
+
+        userEvent.click(loginButton);
+
+        expect(mockRequestSpotifyAuthorizationFlow).toBeCalledTimes(1);
     });
 
 });
